7190b08d7af8a0fed5e829ac9b45343c26a41d92
febri <eko.febriyanto.work@gmail.com>
2025-09-11 14:28:46 +0700

    feat(time-pkg): init time package

---
+++ time/format.go
@@ -0,0 +1,13 @@
+package time
+
+const (
+	IDNZone = "Asia/Jakarta"
+)
+
+const (
+	DateFormat        = "2006-01-02"
+	TimeFormat        = "15:04:05"
+	DateTimeFormat    = "2006-01-02 15:04:05"
+	ISO8601Format     = "2006-01-02T15:04:05Z07:00"
+	IDNDateTimeFormat = "2006-01-02T15:04:05-07:00"
+)
